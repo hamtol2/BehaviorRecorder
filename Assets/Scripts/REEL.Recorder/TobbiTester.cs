32e9a43cc6dbc86ded65af2ef0b956736e3d57a7
Ronnie <ronniej@naver.com>
2018-06-12 14:46:23 +0900

    Added Tobbi Plugin

---
+++ Assets/Scripts/REEL.Recorder/TobbiTester.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.Gaming;
+
+namespace REEL.Recorder
+{
+    public class TobbiTester : MonoBehaviour
+    {
+        public GameObject marker;
+
+        private void Update()
+        {
+            GazePoint gazePoint = TobiiAPI.GetGazePoint();
+            if (CanMove(gazePoint))
+            {
+                MoveMarker(gazePoint);
+            }
+        }
+
+        void MoveMarker(GazePoint gazePoint)
+        {
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(gazePoint.Screen);
+            worldPos.z = -5f;
+
+            marker.transform.position = worldPos;
+        }
+
+        bool CanMove(GazePoint gazePoint)
+        {
+            return !float.IsNaN(gazePoint.Screen.x) && !float.IsNaN(gazePoint.Screen.y);
+        }
+    }
+}
